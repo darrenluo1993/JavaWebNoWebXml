ed3f69bcbe332bfc8ec16f292ae78f24748e7f6b
Darren Luo <darrenluo1993@163.com>
2022-09-27 16:33:39 +0800

    post+application/json方式请求接口并解析请求参数

--- src/main/java/pers/darren/servlet/HelloServlet.java
+++ src/main/java/pers/darren/servlet/HelloServlet.java
@@ -1,10 +1,14 @@
 package pers.darren.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "HelloServlet", urlPatterns = "/hello", loadOnStartup = 1)
 public class HelloServlet extends HttpServlet {
@@ -19,6 +23,15 @@ public class HelloServlet extends HttpServlet {
         System.out.println(this.getClass().getName() + ">>>当前服务器名称:" + req.getServletContext().getAttribute("ServerName"));
 
         String userName = req.getParameter("userName");
+        if (userName == null || userName.isBlank()) {
+            String contentType = req.getContentType();
+            if (contentType.contains("application/json")) {
+                byte[] bytes = req.getInputStream().readAllBytes();
+                String content = new String(bytes, StandardCharsets.UTF_8);
+                JsonObject json = new Gson().fromJson(content, JsonObject.class);
+                userName = json.get("userName").getAsString();
+            }
+        }
         req.getSession().setAttribute("UserName", userName);
 
         resp.getWriter().printf("{\"Hello\":\"%s\"}", userName);
